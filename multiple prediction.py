ff9d6eb9d4827ac1e545686d4b84608e3ebe48af
tong26012546-sketch <tong26012546@gmail.com>
2026-02-16 11:25:12 +0700

    Add files via upload

--- multiple prediction.py
+++ multiple prediction.py
@@ -1,66 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb  9 11:02:53 2026
-
-@author: Lab
-"""
-
 import numpy as np
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+ 
 #import model
 loan_model = pickle.load(open("loan_model.sav",'rb'))
 heart_model = pickle.load(open("heartmodel.sav",'rb'))
-
+Riding_model = pickle.load(open("RidingMowers.sav"'rb'))
+ 
 with st.sidebar:
     selcted = option_menu('Loan and heart prediction',
-                          ['Loan','heart'],
+                          ['Loan','heart','RidingMowers'],
                           default_index=0)
-    
-if(selcted == 'Loan'):
-    st.title("Loan Prediction")
+if(selcted == 'RidingMowers'):
+    st.title("RidingMowers Prediction")
     #input data
-    person_age = st.text_input('person_age')
-    person_gender = st.text_input('person_gender')
-    person_education = st.text_input('person_education')
-    person_income = st.text_input('person_income')
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.text_input('person_home_ownership')
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.text_input('loan_intent')
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.text_input('previous_loan_defaults_on_file')
-    
+    Incoms = st.text_input('Incoms')
+    Lotsize = st.text_input('Lotsize')
     #predict
-    loan_accept = ''
-    
-    if st.button('Loan accept/not test'):
-        loan_accept = loan_model.predict([[
-            float(person_age),
-            float(person_gender),
-            float(person_education),
-            float(person_income),
-            float(person_emp_exp),
-            float(person_home_ownership),
-            float(loan_amnt),
-            float(loan_intent),
-            float(loan_int_rate),
-            float(loan_percent_income),
-            float(cb_person_cred_hist_length),
-            float(credit_score),
-            float(previous_loan_defaults_on_file)
+    RidingMowers = ''
+    if st.button('RidingMowers Owner/nonOwner test'):
+        RidingMowers_accept = Riding_model.predict([[
+            float(Incoms),
+            float(Lotsize)
+ 
         ]])
-        if(loan_accept[0]== 0):
-            loan_accept = 'not accept'
+        if(RidingMowers[0]== 0):
+            RidingMowers = 'not accept'
         else:
-            loan_accept = 'Accept'
-    st.success(loan_accept)
-    
+            RidingMowers = 'Accept'
+    st.success(RidingMowers)
 if(selcted == 'heart'):
     st.title('Heart Prediction')
     age = st.text_input('age')
@@ -76,10 +45,8 @@ if(selcted == 'heart'):
     slope = st.text_input('slope')
     ca = st.text_input('ca')
     thal = st.text_input('thal')
-    
     #predict
     heart_predict = ''
-    
     if st.button('Heart Prediction'):
         heart_predict = heart_model.predict([[
             float(age),
@@ -96,7 +63,6 @@ if(selcted == 'heart'):
             float(ca),
             float(thal)
         ]])
-        
         if(heart_predict[0]== 0):
             heart_predict = 'not have heart Disease'
         else:
